71f971a0770502d260bac9a995ef8403a438a6df
Matias Arenhard <matias_arenhard@hotmail.com>
2024-05-25 15:41:35 -0300

    fix: add order to reports query

--- app/services/generate_csv_report_service.rb
+++ app/services/generate_csv_report_service.rb
@@ -3,7 +3,7 @@ class GenerateCsvReportService
   def call
     CSV.generate do |csv|
       csv << ['Name', 'Email', 'Balance']
-      Person.joins(:user).select('users.email', :name, :balance).each do |client|
+      Person.joins(:user).select('users.email', :name, :balance).order(name: :asc).each do |client|
         csv << [client.name, client.email, client.balance]  
       end
     end
